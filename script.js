189032b66a4ae59b6b188116afebf6661cf1e719
Clifford Morin <cemorin21@gmail.com>
2021-07-02 16:23:50 -0400

    Cliffs chenages

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 // Setting things up
-document.getElementById("helpText").style.display = "none"
 
-// Help button
-const help = document.getElementById("helpText")
-function help() {
-    document.getElementById("helpText").style.display = "block"
-}
+document.addEventListener("DOMContentLoaded", function () {
+  var audio = new Audio("Audio/audio.m4a");
 
-// Intro
-alert("Welcome to Jack Bot's 'brain' code! Here is Jack Bot...")
+  var audio1 = new Audio("audio_file.mp3");
+  // Intro
+  alert("Welcome to Jack Bot's 'brain' code! Here is Jack Bot...");
 
-// My stuff
-const favFood = "outlet"
+  // My stuff
+  const favFood = "outlet";
 
-const favColor = "orange"
+  const favColor = "orange";
 
-const favAnimal = "cat"
+  const favAnimal = "cat";
 
-const favFriend = "Jack Farrell"
+  const favFriend = "Jack Farrell";
 
-
-// The QUESTIONS/CONVERSATION
-alert("Hello there! My name is Jack Bot!")
-const yourName = prompt("What's your name? ")
-alert("Hello " + yourName + "!")
+  // The QUESTIONS/CONVERSATION
+  alert("Hello there! My name is Jack Bot!");
+  const yourName = prompt("What's your name? ");
+  if (!yourName) {
+    alert("You need to put in a name");
+    prompt("What's your name? ");
+  } else {
+    alert("Hello " + yourName + "!");
+  }
+});
