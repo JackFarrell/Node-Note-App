9c629218334dceff554cec16b87ec7e23ddddc17
Jack Farrell <jackfarrell@Jacks-MacBook-Air.local>
2021-07-14 15:33:51 -0400

    test

--- script.js
+++ script.js
@@ -13,10 +13,11 @@ const favFriend = "You"
 
 const favDevice = "Macbook Air"
 
-
 // The QUESTIONS/CONVERSATION
 alert("Hello there! My name is Jack Bot!")
 const yourName = prompt("What's your name? ")
 alert("Hello " + yourName + "!")
 const yourFavFood = prompt("What's your favorite food??? ")
-alert(yourFavFood + "! That sounds sooooooo good! but because right now I am just a computer... my favorite food is an outlet!")
+alert(yourFavFood + "! That sounds sooooooo good! but because right now I am just a computer... my favorite food is a " + favFood + "!")
+prompt("I love it when people take me around on an iPhone! I get to see the world! What's your favorite place to go see?")
+alert("OOOOHH! That sounds so cool! My favorite trip was when my user <i>accidentally<i> flushed me down the toilet!")
